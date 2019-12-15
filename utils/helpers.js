b503991dd0dd70e099474ea5663cabc8dc2af38b
cecil john tantay <cjbtantay@gmail.com>
2019-12-15 11:43:15 -0800

    add common model helpers

---
+++ utils/helpers.js
@@ -0,0 +1,34 @@
+const db = require('../data/dbConfig');
+
+const get = tbl => db(tbl).first();
+
+const findBy = (tbl, filter) =>
+  db(tbl)
+    .where(filter)
+    .first();
+
+const findAllBy = (tbl, filter) => db(tbl).where(filter);
+
+const add = (tbl, item) =>
+  db(tbl)
+    .insert(item)
+    .returning('id');
+
+const update = (tbl, filter, item) =>
+  db(tbl)
+    .where(filter)
+    .update(item);
+
+const remove = (tbl, filter) =>
+  db(tbl)
+    .where(filter)
+    .del();
+
+module.exports = {
+  get,
+  findBy,
+  add,
+  findAllBy,
+  update,
+  remove
+};
