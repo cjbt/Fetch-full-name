ca9c8971cab2c68b5bdd1501b7e26a659ef86d3c
cecil john tantay <cjbtantay@gmail.com>
2019-12-15 12:34:56 -0800

    add integration test to get fullname

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,3 +1,4 @@
+const db = require('../utils/helpers');
 const request = require('supertest');
 const server = require('../server');
 
@@ -14,5 +15,16 @@ describe('/ Test Route', () => {
   it('should return type of text/html', async () => {
     const res = await request(server).get('/');
     expect(res.type).toBe('text/html');
+  });
+});
+
+describe('Get FullName', () => {
+  it('should get full name in the right order (integration)', async () => {
+    const first = await db.get('firstname');
+    const middle = await db.get('middlename');
+    const last = await db.get('lastname');
+    expect(`${first.firstname} ${middle.middlename} ${last.lastname}`).toBe(
+      'Jane Smith Doe'
+    );
   });
 });
