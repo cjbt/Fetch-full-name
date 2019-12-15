e5cddf7b6e0ea8a8a13b561e268ecd5bf5be10e5
cecil john tantay <cjbtantay@gmail.com>
2019-12-15 12:13:28 -0800

    add FirstName Component to fetch data

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FullName from './components/FullName.jsx';
+
+function App() {
+  return (
+    <>
+      <FullName />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/FullName.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const FullName = () => {
+  const [firstname, setFirstname] = useState('');
+  const [middlename, setMiddlename] = useState('');
+  const [lastname, setLastname] = useState('');
+
+  useEffect(() => {
+    fetchFullName();
+  }, []);
+
+  const fetchFullName = async () => {
+    try {
+      const first = await axios.get('http://localhost:8000/firstname');
+      const middle = await axios.get('http://localhost:8000/middlename');
+      const last = await axios.get('http://localhost:8000/lastname');
+      setFirstname(first.data.firstname);
+      setMiddlename(middle.data.middlename);
+      setLastname(last.data.lastname);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(firstname);
+  return (
+    <div>
+      <h1>
+        {firstname} {middlename} {lastname}
+      </h1>
+    </div>
+  );
+};
+
+export default FullName;
